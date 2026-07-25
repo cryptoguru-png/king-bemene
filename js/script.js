82be837ed33d66285fe45a3c3ca28267f3ad14eb
King Bemene <ndine28donald@gmail.com>
2026-07-25 09:47:33 +0100

    Implement community modal open/close functionality
    Added functions to open and close the community modal.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// King Bemene Ecosystem JavaScript
+function openCommunityModal() {
+    document.getElementById("communityModal").style.display = "block";
+}
+
+function closeCommunityModal() {
+    document.getElementById("communityModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("communityModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}// King Bemene Ecosystem JavaScript
